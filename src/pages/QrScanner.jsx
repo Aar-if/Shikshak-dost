8afb5f72da86be76eb7608c0c5e9443fb0cb773e
Arif <arifmajeed007.as@gmail.com>
2023-08-23 12:21:29 +0530

    link update

--- src/pages/QrScanner.jsx
+++ src/pages/QrScanner.jsx
@@ -115,7 +115,7 @@ function QrScanner() {
 
               setTimeout(function () {
                 window.open(
-                  "https://shikshademo.uniteframework.io/teacherapp",
+                  "https://dfh32d43dz5ck.cloudfront.net/teacherapp",
                   "_self"
                 );
               }, 1000);
