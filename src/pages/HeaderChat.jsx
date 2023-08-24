9034aa5cc9e617ffe2e63098e896a43df426e595
siddhi_shnde <siddhi.shnde@tekditechnologies.com>
2023-08-24 12:31:39 +0530

    Add: Chatbot

---
+++ src/pages/HeaderChat.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Flex, Avatar, AvatarBadge, Text } from "@chakra-ui/react";
+import imagePath from "../assets/ChatBot_logo.png";
+const HeaderChat = () => {
+  return (
+	<Flex w="100%">
+  	<Avatar size="lg"  src={imagePath}>
+    	<AvatarBadge boxSize="1.25em" bg="green.500" />
+  	</Avatar>
+  	<Flex flexDirection="column" mx="5" justify="center">
+    	<Text fontSize="lg" fontWeight="bold">
+       AI ChatBot
+    	</Text>
+    	<Text color="green.500">Online</Text>
+  	</Flex>
+	</Flex>
+  );
+};
+
+export default HeaderChat;
